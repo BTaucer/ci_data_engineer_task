726e4d6f23272387acf50a159393b00ad6362420
0036526910 <br52691@fer.hr>
2024-06-03 16:19:41 +0200

    -

--- SQL/third_query.sql
+++ SQL/third_query.sql
@@ -1,8 +1,8 @@
-SELECT players.name, count(players2.name) 
+SELECT players.name, COUNT(players2.name)
 	FROM players
 	LEFT JOIN players AS players2
 	ON players.positions = players2.positions
-	WHERE players.current_club = 'Liverpool' 
-	AND players.age < players2.age 
-	AND players.appearances_current_club > players2.appearances_current_club
-	GROUP BY (players.name)
+	AND players.age > players2.age
+	AND players.appearances_current_club < players2.appearances_current_club
+	WHERE players.current_club = 'Liverpool'
+	GROUP BY (players.name);
